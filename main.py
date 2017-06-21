de5b7d3ec78c761bf44bae425b163d51e5a40509
Unknown <jdabouter@gmail.com>
2017-06-21 14:38:37 +0200

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -9,19 +9,21 @@ import generate_network as gn
 import dynamics_network as dn
 import analyze_network as an
 import networkx as nx
+import time
 
 #NOTE: The print_stuff funtion got moved to the analyze_network file.
 
 # Build a network corresponding to regular grid with d dimensions of size L, 
 # with liquidity threshold -4 and solvency threshold -6
-network = gn.regular_network(L = 100, d = 2, Tl = -4, Ts = -6)
+network = gn.regular_network(L = 30,  d = 3, Tl = -4, Ts = -6)
 
 # Run the simulation for 100 iteration
-avalanche_sizes = dn.run_simulation(network, 1000)
+avalanche_sizes = dn.run_simulation(network, 100)
+end = time.time()
 
 # Plot the distribution of avalanches
-print(avalanche_sizes)
-an.histogram_avalanches(avalanche_sizes, num_bins = 20, y_scale = 'symlog')
+#print(avalanche_sizes)
+an.histogram_avalanches(avalanche_sizes, num_bins = 20, y_scale='symlog', x_scale='symlog')
 
 # Plot the graph in a circle. NOTE: This only works on small graphs.
 # an.plot_network(network)
