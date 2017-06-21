a3607b6f19558f9fc02f20fc3bbc5157350b4707
Unknown <jdabouter@gmail.com>
2017-06-21 03:08:38 +0200

    avalanche propagation first version, done but untested

--- dynamics_network.py
+++ dynamics_network.py
@@ -26,6 +26,12 @@ of the associated nodes, while e[2] gives the dictionary with extra attributes.
 It should also be noted that these names DON'T have to be strings. In fact,
 for the lattice/regular network they're tuples of integers, eg (0,1)
 
+Note: not sure if this is important, because I'm not sure if this situation
+ever arises: if a bank fails while it has a loan extended to a neighbor, 
+that neighbor's capital/liquidity should just increase by that loan I think
+(and the edge should be cleared). Right now this is not implemented as I'm
+assuming this situation never arises, because banks that fail have already
+recovered any debts
 =========================================================================== """
 
 import networkx as nx
@@ -48,9 +54,6 @@ def run_simulation(network, T):
         
         # Check for bankruptcy and propagate infection/failures. If an avalanche happens, its size is appended to avalanche_sizes
         _check_and_propagate_avalanche(network, avalanche_sizes)
-        
-        # After propagation of the avalanche is complete, reset any banks to new, balanced banks
-        _reset_bankrupt_banks(network)
                     
     # Return the list of avalanche sizes
     return avalanche_sizes
@@ -143,48 +146,48 @@ def _invest_surplus_liquidity(network):
     - avalanche: cascade of failures
     - infection: cascade of banks trying to regain balance by asking money from borrowers '''
 def _check_and_propagate_avalanche(network, avalanche_sizes):
-    a_bank_is_bankrupt = False  # Is any bank bankrupt?
-    bankrupt_banks = []  # list of names currently bankrupt banks (used during avalanche propagation)
+    # If any bank has gone bankrupt, start an infection. Also get a list of bankrupt banks
+    infection_spreading, bankrupt_banks = __find_bankruptcies(network)  # list of bankrupt banks is a list of names
+    current_infected = []  # infected is a list of names
     
-    # Is any bank bankrupt?
-    a_bank_is_bankrupt, bankrupt_banks = __find_bankruptcies(network)
-    if a_bank_is_bankrupt:  # A new avalanche has started, record it
-        avalanche_sizes.append(0)
-            
-    # While any bank is bankrupt, propagate the failure avalanche
-    while a_bank_is_bankrupt:
-        for node in bankrupt_banks:
-            # Make list of neighbors that loaned this bank money (we call them 'infected')
-            infected = []
-            my_edges = network.edges([node[0]], data=True)
-            for edge in my_edges:
-                if edge[2]['debt'] > 0 and edge[2]['borrower'] == node[0]:  # If this node borrowed from someone
-                    infected.append(edge[2]['loaner'])  # Add that someone to the infected
-                    
-            # For each 'infected' neighbor, have them try to get their lost money back
-            for node in infected:
-                pass  # UNFINISHED
-            
-        # See if any more banks are bankrupt
+    while infection_spreading:
+        # For each bankrupt bank, determine if any of their neighbors are infected, and how much capital they lost  
+        for bankrupt_bank in bankrupt_banks:
+            for neighbor in network.neighbors(bankrupt_bank):
+                our_edge = network.edge[bankrupt_bank][neighbor]
+                # If there is a debt, and the neighbor was the loaner, add this neighbor to the infected
+                if our_edge['debt'] != 0 and our_edge['loaner'] == neighbor:
+                    current_infected.append(neighbor)
+                    # Remove the loaned money from the infected neighbor's capital
+                    network.node[neighbor]['capital'] -= our_edge['debt']
+                    # Reset the edge
+                    __reset_edge(our_edge)
+
+        # For each of the infected, try to recover losses
+        new_infected = []
+        for node in current_infected:
+            for neighbor in network.neighbors(node):
+                our_edge = network.edge[node][neighbor]
+                # If the infected node loaned money to this neighbor
+                if our_edge['debt'] != 0 and our_edge['loaner'] == node:
+                    # recover that money
+                    network.node[node]['liquidity'] += our_edge['debt']
+                    # Subtract from the neighbor
+                    network.node[neighbor]['liquidity'] -= our_edge['debt']
+                    # Reset the edge
+                    __reset_edge(our_edge)
+                    # Add the neighbor to newly infected
+                    new_infected.append(neighbor)
+
+        # Reset currently bankrupt banks. Check for any new bankruptcies
+        _reset_bankrupt_banks(network)  # NOTE: this function now iterates through the entire network when it could just be passed bankrupt_banks. Fix/optimize this later
         a_bank_is_bankrupt, bankrupt_banks = __find_bankruptcies(network)
         
-        # Increment the current avalanche
-        avalanche_sizes[-1] += 1
-
-''' After avalanche propagation is done, reset bankrupt banks to balanced bankss '''
-def _reset_bankrupt_banks(network):
-    for node in network.nodes_iter(data=True):  
-        if node[1]['bankrupt']:
-            # Reset the capital
-            node[1]['capital'] = 0
-            node[1]['liquidity'] = 0
-            # Reset the associated edges. NOTE: node[0] is the NAME of this node (in string format)
-            for edge in network.edges([node[0]]):
-                network[edge[0]][edge[1]]['debt'] = 0  # network[node1][node2] gives the associated edge. This way of modifying the edge seems contrived and like it could be done easier, but I read that modifying the edge directly is a bad idea here: http://networkx.readthedocs.io/en/networkx-1.11/tutorial/tutorial.html#accessing-edges
-                network[edge[0]][edge[1]]['loaner'] = None
-                network[edge[0]][edge[1]]['borrower'] = None
-            # no longer bankrupt
-            node[1]['bankrupt'] = False
+        # Evaluate if the infection spreading is finished or not
+        infection_spreading = False
+        if a_bank_is_bankrupt or len(new_infected) != 0:
+            infection_spreading = True
+            current_infected = new_infected
 
 
 
@@ -193,7 +196,13 @@ def _reset_bankrupt_banks(network):
 HELPER FUNCTIONS
 =========================================================================== '''
 
-''' helper function for determining if a given node has a debt. Returns 
+''' Helper function for resetting an edge '''
+def __reset_edge(edge):
+    edge['debt'] = 0
+    edge['borrower'] = None
+    edge['loaner'] = None
+
+''' Helper function for determining if a given node has a debt. Returns 
     True/False and returns a dict with all nodes who loaned money to the given 
     node, and how much. '''
 def __find_loaners(network, node_name):
@@ -209,7 +218,7 @@ def __find_loaners(network, node_name):
             loaners[edge[2]['loaner']] = edge[2]['debt']
     return in_debt, loaners
 
-''' helper function for determining if a given node has neighbors with negative
+''' Helper function for determining if a given node has neighbors with negative
     liquidity. Returns True/False, and a dict with the broke neighbors and 
     the values of their negative liquidities. '''
 def __find_broke_neighbors(network, node_name):
@@ -225,17 +234,29 @@ def __find_broke_neighbors(network, node_name):
             broke_neighbors[n] = liquidity
     return have_broke_neighbors, broke_neighbors
 
-''' helper function for checking if any banks are now bankrupt '''           
+''' Helper function for checking if any banks are now bankrupt '''           
 def __find_bankruptcies(network):
     a_bank_is_bankrupt = False
     bankrupt_banks = []
     for node in network.nodes_iter(data=True):
         # Check whether this node is bankrupt
         if node[1]['capital'] <= network.graph['Ts'] or node[1]['liquidity'] <= network.graph['Tl']:
-            # If this node is already set to bankrupt, then we've already considered it during avalanche propagation.
-            if not node[1]['bankrupt']:
-                # If this node exceeded either threshold but isn't bankrupt yet, return true and append to output
-                node[1]['bankrupt'] = True
-                bankrupt_banks.append(node)
-                a_bank_is_bankrupt = True
+            node[1]['bankrupt'] = True
+            bankrupt_banks.append(node[0])
+            a_bank_is_bankrupt = True
     return a_bank_is_bankrupt, bankrupt_banks
+
+''' Helper function for resetting all bankrupt banks '''
+def _reset_bankrupt_banks(network):
+    for node in network.nodes_iter(data=True):  
+        if node[1]['bankrupt']:
+            # Reset the capital
+            node[1]['capital'] = 0
+            node[1]['liquidity'] = 0
+            # Reset the associated edges. NOTE: node[0] is the NAME of this node (in string format)
+            for edge in network.edges([node[0]]):
+                network[edge[0]][edge[1]]['debt'] = 0  # network[node1][node2] gives the associated edge. This way of modifying the edge seems contrived and like it could be done easier, but I read that modifying the edge directly is a bad idea here: http://networkx.readthedocs.io/en/networkx-1.11/tutorial/tutorial.html#accessing-edges
+                network[edge[0]][edge[1]]['loaner'] = None
+                network[edge[0]][edge[1]]['borrower'] = None
+            # no longer bankrupt
+            node[1]['bankrupt'] = False
--- main.py
+++ main.py
@@ -10,25 +10,20 @@ import dynamics_network as dn
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def print_stuff():
+    print('\n\n\n')
+    for node in network.nodes_iter(data=True):
+        print(node)
+    print()
+    for edge in network.edges_iter(data=True):
+        print(edge)
+
 # Build a network corresponding to regular grid with d dimensions of size L, 
 # with liquidity threshold -4 and solvency threshold -6
-network = gn.regular_network(L = 3, d = 2, Tl = -4, Ts = -6)
+network = gn.regular_network(L = 5, d = 2, Tl = -4, Ts = -6)
 
-# DEBUG
-dn._perturb(network)
-for node in network.nodes_iter(data=True):
-    print(node)
-print()
-for edge in network.edges_iter(data=True):
-    print(edge)
-    
-dn._invest_surplus_liquidity(network)
-print("\n\n\n")
-for node in network.nodes(data=True):
-    print(node)
-print()
-for edge in network.edges_iter(data=True):
-    print(edge)
+print_stuff()
+
 
 # Run the simulation for 1 iteration
 #dn.run_simulation(network, 1)
@@ -37,3 +32,5 @@ for edge in network.edges_iter(data=True):
 # takes too long for me to draw 100x100
 #nx.draw(G)
 #plt.show()
+
+
