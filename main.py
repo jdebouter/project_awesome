751e8eb90d60746a8dddaa322d3244aac0fc0297
Jonathan <jdabouter@gmail.com>
2017-06-25 20:58:44 +0200

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -7,31 +7,36 @@ import dynamics_network as dn
 import analyze_network as an
 import networkx as nx
 
-## Build a network 
-network = gn.regular_network(L = 100, d = 2, Tl = -4, Ts = -6)
-
 ''' Parameters that change the implementation a little:
     quick_repaying = True/False. True means that banks will repay a debt if 
         that happen to get a surplus that round, even if they have negative 
         liquidity. False means that they'll only repay if they have positive 
         capital/liquidity 
+    (UNFINISHED)
     transfer_pattern - node_by_node/evenly_distributed. 'node_by_node' means
         that some random node is chosen and all debt/loan is repayed / collected
         until balance is regained. 'evenly_distributed' means that they'll try
         to repay/collect from all borrowers/lenders evenly
-    loan_initiator = surplus/deficit. 'surplus' means that nodes with extra
-        capital/liqiudity will look for broke neighbors to extend loans.
-        'deficit' means that nodes with negative capital/liquidity will look
-        for rich neighbors to ask for loans
+    (UNFINISHED)
     infection_collect = all/lost_money. 'all' means that nodes collect all 
         loans back after infection. 'lost_money' means that they only collect
         back any money that was lost. '''
 parameters = {"quick_repaying" : False,
-              "transfer_pattern" : "distributed_evenly",
-              "loan_initiator" : "surplus",
+              "transfer_pattern" : "node_by_node",
               "infection_collect" : "all"}
 
-avalanche_sizes = dn.run_simulation(network, 100, parameters)
+## Build a network 
+network = gn.regular_network(L = 3, d = 2, Tl = -4, Ts = -6)
+#avalanche_sizes = dn.run_simulation(network, 500, parameters)
+
+#avalanche_sizes = []
+#for i in range(1):
+#    network = gn.regular_network(L = 10, d = 4, Tl = -7, Ts = -10)
+#    sizes = dn.run_simulation(network, 1000, parameters)
+#    # Remove first 10%
+#    sizes = sizes[int(len(sizes) / 10.0) : ]
+#    avalanche_sizes = avalanche_sizes + sizes
 
 ## Plot the distribution of avalanches
-an.histogram_avalanches(avalanche_sizes, num_bins = 20, y_scale='log', x_scale='linear')
+#an.histogram_avalanches(avalanche_sizes, num_bins = 100, y_scale='log', x_scale='linear')
+#an.histogram_avalanches(avalanche_sizes, num_bins = 100, y_scale='log', x_scale='log')
