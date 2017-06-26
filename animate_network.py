e10f42164946cea536cdf204987147c89524431d
Jed <dzedcpt@gmail.com>
2017-06-26 14:15:13 +0200

    Update simulation animations

--- animate_network.py
+++ animate_network.py
@@ -65,16 +65,20 @@ def gen_graph(network, defaults):
     nx.set_node_attributes(to_return, 'bankrupt', 0) 
 
     nx.set_edge_attributes(to_return, 'debt', 0) 
+    nx.set_edge_attributes(to_return, 'color', 'k') 
     for n in network.nodes():
         to_return.add_node(n)
         to_return.node[n]['capital'] = n.getCapital()
         to_return.node[n]['liquidity'] = n.getLiquidity()
         if n.getLiquidity() < 0:
-            to_return.node[n]['color'] = '0.75'
+            # to_return.node[n]['color'] = 'palegreen'
+            to_return.node[n]['color'] = -1
         elif n.getLiquidity() > 0:
-            to_return.node[n]['color'] = '0.75'
+            # to_return.node[n]['color'] = 'palegreen'
+            to_return.node[n]['color'] = 1
         else:
-            to_return.node[n]['color'] = '0.75'
+            # to_return.node[n]['color'] = 'palegreen'
+            to_return.node[n]['color'] = 0
 
         if n in defaults:
             print ("helloj w")
@@ -89,13 +93,14 @@ def gen_graph(network, defaults):
             if debt > 0:
                 to_return.add_edge(*(n, i))
                 to_return[n][i]['debt'] = debt
+                to_return[n][i]['color'] = 'k'
     return to_return
      
 
 def graph_plot(network, step, node_size, font_size, label_edges = True):
     dn.run_simulation(network, step)
     
-    G = gen_graph(network)
+    G = gen_graph(network, [])
     # edges = G.edges()
 
     fig = plt.figure(figsize=(5,5))
@@ -117,11 +122,11 @@ def graph_plot(network, step, node_size, font_size, label_edges = True):
     node_labels = nx.draw_networkx_labels(G, pos, node_label, font_size=font_size)
 
     plt.axis('off')
-    # plt.show()
+    plt.show()
     
 
 
-def animate_simulation(network, node_size, fast = True):
+def animate_simulation(network, node_size):
     dn.step_simulation(network)
 
     fig = plt.figure(figsize=(5,5))
@@ -144,11 +149,112 @@ def animate_simulation(network, node_size, fast = True):
         fig.gca().set_xlim(left=-1.1, right=1.1)
         fig.gca().set_ylim(bottom=-1.1, top=1.1)
         plt.axis("off")
-        # if fast:
+
         defaults = dn.step_simulation(network)
         plt.title(str(n))
-        print (np.ndarray.flatten(np.array(defaults)))
-        # if defaults != []:
+        G = gen_graph(network, defaults)
+        node_color = [G.node[n]['color'] for n in G.nodes()]
+
+        edges = nx.draw_networkx_edges(G, pos,alpha=.3, edge_color='k') 
+        edges2 = nx.draw_networkx_edges(network, pos, alpha=.1, edge_color='k') 
+        nodes = nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color=node_color)
+
+        return nodes, edges, edges2
+
+    ani = FuncAnimation(fig, update, interval=1000)
+    plt.show()
+   
+def default_graph(defaults, network):
+    G = nx.DiGraph()
+    
+    for n in defaults:
+        G.add_node(n)
+
+    for n in G.nodes():
+        for i in network.neighbors(n):
+            if G.has_node(i):
+                G.add_edge(*(n, i))
+    starts = []
+    for n in G.nodes():
+        if G.in_degree(n) == 0:
+            starts.append(n)
+
+    to_return = []
+    for n in starts:
+        to_return.append(n)
+        z = []
+        z.append(n)
+        while (len(z) > 0):
+            h = z.pop(0)
+            for i in G.neighbors(h):
+                z.append(i)
+                to_return.append(i)
+    print (len(to_return))
+    # nx.draw(G)
+    # plt.show()
+    return to_return
+
+def animate_defaults(network, avalanche_size, node_size):
+    dn.step_simulation(network)
+    defaults = []
+    while (len(defaults) < avalanche_size):
+        G = gen_graph(network, [])
+        defaults = dn.step_simulation(network)
+        defaults = set(defaults.tolist())
+
+    d = default_graph(defaults, G)
+
+    fig = plt.figure(figsize=(5,5))
+    fig.gca().set_xlim(left=-1.1, right=1.1)
+    fig.gca().set_ylim(bottom=-1.1, top=1.1)
+    plt.axis('off')
+
+    for n in G.nodes():
+        G.node[n]['color'] = 'g'
+    pos = nx.shell_layout(G, scale=1)
+
+    edges = nx.draw_networkx_edges(G, pos, alpha=.3, edge_color='k',  vmin=-1, vmax=1) 
+    edges2 = nx.draw_networkx_edges(network, pos, alpha=.3, edge_color='k',  vmin=-1, vmax=1) 
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color='w', vmin=-1, vmax=1)
+    
+    def update(n):
+        z = n % 2
+        if z == 0:
+            x = d[0]
+            G.node[x]['color'] = 'r'
+        elif len(d) == 0:
+            pass
+        else:
+            x = d.pop(0)
+            for i in G.neighbors(x):
+                G[x][i]['color'] = 'r'
+
+
+        fig.clf()
+        fig.gca().set_xlim(left=-1.1, right=1.1)
+        fig.gca().set_ylim(bottom=-1.1, top=1.1)
+        plt.axis("off")
+
+        plt.title(str(n))
+        node_color = [G.node[n]['color'] for n in G.nodes()]
+        edge_color = [G[u][v]['color'] for u, v in G.edges()]
+
+        edges = nx.draw_networkx_edges(G, pos,alpha=.3, edge_color=edge_color) 
+        edges2 = nx.draw_networkx_edges(network, pos, alpha=.1, edge_color='k') 
+        nodes = nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color=node_color)
+
+        return nodes, edges, edges2
+
+    ani = FuncAnimation(fig, update, interval=1000, frames=len(defaults))
+    plt.show()
+ 
+G = gn.regular_network(L = 7, d = 2, Tl = -4, Ts = -6)
+# G = gn.barabasi_albert_network(2, 2, Tl=-4, Ts=-2)
+# animate_simulation(G, 20)
+# graph_plot(G, 5,120, 10)
+animate_defaults(G, 10, 15)
+
+# if defaults != []:
             # print ("hello")
         # else:
             # z = n % 4
@@ -168,42 +274,4 @@ def animate_simulation(network, node_size, fast = True):
             # else:
                 # assert(False)
  
-        G = gen_graph(network, defaults)
-        node_color = [G.node[n]['color'] for n in G.nodes()]
-
-        edges = nx.draw_networkx_edges(G, pos,alpha=.3, edge_color='k',  vmin=-1, vmax=1) 
-        edges2 = nx.draw_networkx_edges(network, pos, alpha=.1, edge_color='k',  vmin=-1, vmax=1) 
-        nodes = nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color=node_color, cmap='tab20c', vmin=-1, vmax=1)
-
-        return nodes, edges, edges2
-
-    ani = FuncAnimation(fig, update, interval=1000)
-    plt.show()
-    
-
-G = gn.regular_network(L = 5, d = 2, Tl = -4, Ts = -6)
-# G = gn.barabasi_albert_network(2, 2, Tl=-4, Ts=-2)
-animate_simulation(G, 20)
-# graph_plot(G, 20,120, 10)
-# plt.savefig('1.png')
-# graph_plot(G, 1,120, 10)
-# plt.savefig('2.png')
-# graph_plot(G, 1,120, 10)
-# plt.savefig('3.png')
-# graph_plot(G, 1,120, 10)
-# plt.savefig('4.png')
-# graph_plot(G, 1,120, 10)
-# graph_plot(G, 20,120, 10)
-
-# fig = plt.figure()
-# d = []
-# for i in range(10):
-    # G = nx.barabasi_albert_graph(10, 2)
-    # z = nx.draw(G, animated=True)
-    # print (z)
-    # d.append(z)
-
-# ani = animation.ArtistAnimation(fig, d, interval=50, blit=True)
-# plt.show()
-
 
